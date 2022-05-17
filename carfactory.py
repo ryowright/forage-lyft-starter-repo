76e75cf5d8514abe3a48faf449b10033716ae4d4
ryowright <ryow.college@gmail.com>
2022-05-17 13:04:34 -0700

    made CarFactory class methods static and refactored unit tests

--- carfactory.py
+++ carfactory.py
@@ -9,27 +9,32 @@ from battery.spindler_battery import SpindlerBattery
 
 
 class CarFactory:
-  def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+  @staticmethod
+  def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
     engine = CapuletEngine(current_mileage, last_service_mileage)
     battery = SpindlerBattery(last_service_date, current_date)
     return Car(engine, battery)
   
-  def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+  @staticmethod
+  def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
     engine = WilloughbyEngine(current_mileage, last_service_mileage)
     battery = SpindlerBattery(last_service_date, current_date)
     return Car(engine, battery)
 
-  def create_palindrome(self, current_date, last_service_date, warning_light_on):
+  @staticmethod
+  def create_palindrome(current_date, last_service_date, warning_light_on):
     engine = SternmanEngine(warning_light_on)
     battery = SpindlerBattery(last_service_date, current_date)
     return Car(engine, battery)
 
-  def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+  @staticmethod
+  def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
     engine = WilloughbyEngine(current_mileage, last_service_mileage)
     battery = NubbinBattery(last_service_date, current_date)
     return Car(engine, battery)
 
-  def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+  @staticmethod
+  def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
     engine = CapuletEngine(current_mileage, last_service_mileage)
     battery = NubbinBattery(last_service_date, current_date)
     return Car(engine, battery)
